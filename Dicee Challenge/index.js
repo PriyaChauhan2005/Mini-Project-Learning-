5bc674ab050b19544966d5ffd44942524cae8bb4
PRIYA CHAUHAN <priyachauhan15162@gmail.com>
2026-01-26 23:43:52 +0530

    Dice Challenge

---
+++ Dicee Challenge/index.js
@@ -0,0 +1,20 @@
+function rollDice() {
+  // Generate random number for Player 1
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomImageSource = "images/dice" + randomNumber1 + ".png";
+  document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+  // Generate random number for Player 2
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+  document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+  // Decide the winner
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h3").innerHTML = "🚩 Player 1 Wins!";
+  } else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h3").innerHTML = "Player 2 Wins! 🚩";
+  } else {
+    document.querySelector("h3").innerHTML = "Draw!";
+  }
+}
